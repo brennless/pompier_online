8d959293d4418ebfab6904d3f69efbdbfdd14c8c
Alois Maier <alois.maier@bordeaux-inp.fr>
2025-10-26 14:56:01 +0100

    premiere version du site, test

---
+++ public/script.js
@@ -0,0 +1,27 @@
+const socket = new WebSocket('ws://192.168.1.135:3000');
+socket.addEventListener('open', event => {
+  console.log('WebSocket connection established!');
+  // Sends a message to the WebSocket server
+		if(window.location.pathname == "/admin"){ socket.send("admin") }
+		else { socket.send("client") }
+});
+
+console.log(window.location.pathname);
+if(window.location.pathname === "/admin"){
+		const btn_admin = document.getElementById("btn_admin");
+		btn_admin.addEventListener("click", () => {
+				socket.send("L'ADMIN A CLIQUE")
+		})
+}
+else{
+		console.log("On est pas sur la page admin");
+
+socket.addEventListener("message", event => {
+		const titre = document.getElementById("Titre");
+		console.log(event.data)
+		if(event.data.toString() == "change vite"){
+				titre.style.color = "red";
+				console.log("l'admin a bien cliqué");
+		}
+})
+}
---
+++ server.js
@@ -0,0 +1,55 @@
+import express from 'express';
+import { WebSocketServer } from 'ws';
+import http from 'http';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Pour pouvoir utiliser __dirname avec les modules ES :
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = 3000;
+
+// Creation d'un serveur http à partir d’Express
+const server = http.createServer(app);
+
+// Creation serveur WebSocket base sur le serv http
+const wss = new WebSocketServer({ server });
+
+// A la connection ws
+wss.on('connection', ws => {
+		console.log('Client WebSocket connecté');
+		ws.on('message', message => {
+				console.log('Message reçu :', message.toString());
+				if(message == "L'ADMIN A CLIQUE"){
+						console.log("La condition marche");
+						wss.clients.forEach(client => {
+								if(client.readyState === WebSocket.OPEN){
+										client.send("change vite");
+								}
+						});
+				}
+		});
+
+		ws.send('Salut du serveur WebSocket');
+});
+
+// Servir les fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Page de base
+app.get('/', (req, res) => {
+		res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Page admin
+app.get('/admin', (req, res) => {
+		res.sendFile(path.join(__dirname, 'public', 'admin.html'));
+});
+
+// On lance le serv
+server.listen(PORT, () => {
+		console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
+
