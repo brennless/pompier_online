ff1bcd25dbc95d9894b66d32b771f18945f852ab
Alois Maier <alois.maier@bordeaux-inp.fr>
2025-10-27 21:55:27 +0100

    max pni

--- public/script.js
+++ public/script.js
@@ -29,7 +29,7 @@ form.addEventListener('submit', (event) => {
 		const poulsMaxV = document.getElementById('max_pouls').value;
 		const spo2MaxV = document.getElementById('max_spo2').value;
 		const tempMaxV = document.getElementById('max_temp').value;
-		// const pniMaxV1 = document.getElementById('pni_entry1').value;
+		const pniMaxV1 = document.getElementById('max_pni1').value;
 		// const pniMaxV2 = document.getElementById('pni_entry2').value;
 		const data = {
 				pouls : poulsV,
@@ -39,9 +39,9 @@ form.addEventListener('submit', (event) => {
 				temp : tempV,
 				tempM : tempMaxV,
 				pni1 : pniV1,
-				pni2 : pniV2/*,
+				pni2 : pniV2,
 				pni1M : pniMaxV1,
-				pni2M : pniMaxV2*/
+				//pni2M : pniMaxV2
 		}
 		console.log(data);
 		socket.send(JSON.stringify(data));
@@ -70,7 +70,7 @@ socket.addEventListener("message", event => {
 				more_than_max(data.pouls, data.poulsM, '.pouls');
 				more_than_max(data.spo2, data.spo2M, '.spo2');
 				more_than_max(data.temp, data.tempM, '.temp');
-				//more_than_max(data, '.pouls');
+				more_than_max(data.pni1, data.pni1M '.pni');
 		} catch (error) {
 				console.log('Oops : ', error);
 				console.log('Message : ', event.data.toString());
